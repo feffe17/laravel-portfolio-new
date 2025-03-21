2be804690015a8623c0fd116a788c9230fd88b22
feffe1702 <fpm.asd@gmail.com>
2025-03-21 13:09:31 +0100

    Add Vite and Mix integration for JavaScript in admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @vite(['resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -10,5 +11,7 @@
     <div class="container">
         @yield('content')
     </div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+
 </body>
 </html>
