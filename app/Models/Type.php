509990d694a173f43f346e533fee8da3cc30ff43
feffe1702 <fpm.asd@gmail.com>
2025-03-24 09:08:52 +0100

    Add Type model enhancements and migration for type_id in projects table

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Type extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name']; // Assicurati che il campo "name" sia riempiibile
+
+    // Relazione one-to-many con il modello Project
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
